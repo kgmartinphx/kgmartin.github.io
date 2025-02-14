cb9a92c5fed564a674734d505daf5b52a2b10279
kgmartinphx <161514415+kgmartinphx@users.noreply.github.com>
2025-02-14 11:35:10 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const popup = document.getElementById("popup");
+const finalMessage = document.getElementById("finalMessage");
+const noButton = document.getElementById("noButton");
+const yesButton = document.getElementById("yesButton");
+
+const messages = [
+    "Are you sure?",
+    "Really? Think again!",
+    "Come on, give me a chance!",
+    "You're breaking my heart!",
+    "Please? Pretty please?",
+    "I'll keep asking forever!",
+    "Just click yes already!",
+    "You're too cute to say no",
+    "I made this popup just for you",
+    "I won't stop until you say yes",
+	"Dont be a bitch",
+	"Ima just make this loop forever until you say yes"
+];
+
+let messageIndex = 0;
+
+noButton.addEventListener("click", () => {
+    noButton.textContent = messages[messageIndex % messages.length];
+    messageIndex++;
+});
+
+yesButton.addEventListener("click", () => {
+    popup.style.display = "none";
+    finalMessage.style.display = "block";
+});
